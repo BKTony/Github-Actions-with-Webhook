2e4ceac85ed7e3f9ade5a7fd11a4928ebffda541
BKTony <94571866+BKTony@users.noreply.github.com>
2023-02-21 15:04:55 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,24 +1,14 @@
 import requests
+import json
 import os
 
-url = os.environ['WEBHOOK_URL']  # 从命令行参数中获取 Webhook URL
+webhook_url = os.environ['WEBHOOK_URL']
 
-# payload = {"text": "今日已收盘!"}
+def send_message(message):
+    headers = {'Content-Type': 'application/json'}
+    data = {'msgtype': 'text', 'text': {'content': message}}
+    r = requests.post(url=webhook_url, headers=headers, data=json.dumps(data))
+    print(r.text)
 
-# response = requests.post(url, json=payload)
-
-# if response.status_code == 200:
-#   print("Webhook sent successfully!")
-# else:
-#   print("Failed to send webhook.")
-
-def send_message():
-    message = '今日已收盘!'
-    data = {
-        'text': message
-    }
-    response = requests.post(url, json=data)
-    if response.ok:
-        print('Message sent!')
-    else:
-        print('Message failed!')
+if __name__ == '__main__':
+    send_message('Hello from GitHub Actions!')
