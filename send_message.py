9bf399f135d8f57b0421e949e0387f773244e71a
BKTony <94571866+BKTony@users.noreply.github.com>
2023-02-21 15:24:45 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # webhook_url = os.environ['WEBHOOK_URL']
-url = “https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=591a4378-8d1c-4944-83b9-60162b434f20”
+url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=591a4378-8d1c-4944-83b9-60162b434f20"
 webhook_url = requests.get(url)
     
 def send_message(message):
