139faa6c7451bc4512797a0bfab8d564f4bdd487
BKTony <94571866+BKTony@users.noreply.github.com>
2023-02-21 13:47:34 +0800

    Update and rename send_webhook.py to send_message.py

---
+++ .github/workflows/send_message.py
@@ -0,0 +1,25 @@
+
+import requests
+import os
+
+url = os.environ['WEBHOOK_URL']  # 从命令行参数中获取 Webhook URL
+
+# payload = {"text": "今日已收盘!"}
+
+# response = requests.post(url, json=payload)
+
+# if response.status_code == 200:
+#   print("Webhook sent successfully!")
+# else:
+#   print("Failed to send webhook.")
+
+def send_message():
+    message = '今日已收盘!'
+    data = {
+        'text': message
+    }
+    response = requests.post(url, json=data)
+    if response.ok:
+        print('Message sent!')
+    else:
+        print('Message failed!')
